07d5043747639b355b82c3da2a45396bc8025d80
JamesHagerty1 <70479852+JamesHagerty1@users.noreply.github.com>
2024-12-05 08:37:46 -0500

    new approach to adding bootstrap and css styles

--- nextjs-react-jsonschema/src/app/page.js
+++ nextjs-react-jsonschema/src/app/page.js
@@ -1,10 +1,11 @@
-/* 
-Needs to be a Client Component to use rjsf imports
-*/
+// Needs to be a Client Component to use rjsf imports
 'use client';
 
 import Form from '@rjsf/core';
 import validator from '@rjsf/validator-ajv8';
+// Need bootstrap styles for rjsf Form to look right
+import 'bootstrap/dist/css/bootstrap.css'
+import './styles/rjsf.css'
 
 const schema = {
   title: 'Test form',
@@ -19,12 +20,23 @@ const schema = {
   },
 };
 
+// Gives divs for individual fields a class, so I can apply styles to them
+const uiSchema = {
+  name: {
+    'ui:classNames': 'custom-class-name',
+  },
+  age: {
+    'ui:classNames': 'custom-class-age',
+  },
+};
+
 export default function Home() {
   return (
     <div>
       <div>Hi!!</div>
       <Form
         schema={schema}
+        uiSchema={uiSchema}
         validator={validator}
       />
     </div>
