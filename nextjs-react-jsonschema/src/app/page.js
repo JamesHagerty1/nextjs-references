06db9a6daf6f0eefd7179dc57f83c7d234107990
JamesHagerty1 <70479852+JamesHagerty1@users.noreply.github.com>
2024-12-05 05:12:36 -0500

    working jsonschema form mvp

--- nextjs-react-jsonschema/src/app/page.js
+++ nextjs-react-jsonschema/src/app/page.js
@@ -1,5 +1,35 @@
+/* 
+Needs to be a Client Component to use rjsf imports
+*/
+'use client';
+
+import Form from '@rjsf/core';
+import { RJSFSchema } from '@rjsf/utils';
+import validator from '@rjsf/validator-ajv8';
+
+const schema = {
+  title: 'Todo',
+  type: 'object',
+  required: ['title'],
+  properties: {
+    title: { type: 'string', title: 'Title', default: 'A new task' },
+    done: { type: 'boolean', title: 'Done?', default: false },
+  },
+};
+
+const log = (type) => console.log.bind(console, type);
+
 export default function Home() {
   return (
-    <div>Hello</div>
+    <div>
+      <div>Hi</div>
+      <Form
+        schema={schema}
+        validator={validator}
+        onChange={log('changed')}
+        onSubmit={log('submitted')}
+        onError={log('errors')}
+      />
+    </div>
   );
 }
