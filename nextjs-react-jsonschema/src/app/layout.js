72a0c5218ca4c3131ba43ec5301bbc5b4774d0f0
JamesHagerty1 <70479852+JamesHagerty1@users.noreply.github.com>
2024-12-05 05:56:51 -0500

    simpler form, and bootstrap link moved to layout

--- nextjs-react-jsonschema/src/app/layout.js
+++ nextjs-react-jsonschema/src/app/layout.js
@@ -9,6 +9,14 @@ export default function RootLayout({ children }) {
       <body
         className={``}
       >
+        {/* Bootstrap required for rjsf default Form styles */}
+        <link 
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" 
+          rel="stylesheet" 
+          integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" 
+          crossOrigin="anonymous"
+        >
+        </link>
         {children}
       </body>
     </html>
--- nextjs-react-jsonschema/src/app/page.js
+++ nextjs-react-jsonschema/src/app/page.js
@@ -4,38 +4,28 @@ Needs to be a Client Component to use rjsf imports
 'use client';
 
 import Form from '@rjsf/core';
-import { RJSFSchema } from '@rjsf/utils';
 import validator from '@rjsf/validator-ajv8';
 
 const schema = {
-  title: 'Todo',
+  title: 'Test form',
   type: 'object',
-  required: ['title'],
   properties: {
-    title: { type: 'string', title: 'Title', default: 'A new task' },
-    done: { type: 'boolean', title: 'Done?', default: false },
+    name: {
+      type: 'string',
+    },
+    age: {
+      type: 'number',
+    },
   },
 };
-
-const log = (type) => console.log.bind(console, type);
 
 export default function Home() {
   return (
     <div>
-      <div>Hi</div>
-      <link 
-        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" 
-        rel="stylesheet" 
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" 
-        crossOrigin="anonymous"
-      >
-      </link>
+      <div>Hi!!</div>
       <Form
         schema={schema}
         validator={validator}
-        onChange={log('changed')}
-        onSubmit={log('submitted')}
-        onError={log('errors')}
       />
     </div>
   );
