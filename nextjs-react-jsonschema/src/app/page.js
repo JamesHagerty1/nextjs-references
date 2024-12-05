8c3b44235bbaa83f98f003589617ea772406f071
JamesHagerty1 <70479852+JamesHagerty1@users.noreply.github.com>
2024-12-05 05:27:50 -0500

    bootstrap styles needed for form

--- nextjs-react-jsonschema/src/app/page.js
+++ nextjs-react-jsonschema/src/app/page.js
@@ -23,6 +23,13 @@ export default function Home() {
   return (
     <div>
       <div>Hi</div>
+      <link 
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" 
+        rel="stylesheet" 
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" 
+        crossOrigin="anonymous"
+      >
+      </link>
       <Form
         schema={schema}
         validator={validator}
