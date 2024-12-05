1a52f6e10806b171fb787847980faea635adb414
JamesHagerty1 <70479852+JamesHagerty1@users.noreply.github.com>
2024-12-05 06:02:23 -0500

    doc

--- nextjs-react-jsonschema/src/app/layout.js
+++ nextjs-react-jsonschema/src/app/layout.js
@@ -9,7 +9,7 @@ export default function RootLayout({ children }) {
       <body
         className={``}
       >
-        {/* Bootstrap required for rjsf default Form styles */}
+        {/* Bootstrap required for rjsf/core default Form styles */}
         <link 
           href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" 
           rel="stylesheet" 
