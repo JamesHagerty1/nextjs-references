03c8d73db86189be71c34f457f5bb27736470725
JamesHagerty1 <70479852+JamesHagerty1@users.noreply.github.com>
2024-12-05 02:38:33 -0500

    any type for root page, try to avoid type issue on vercel

--- nextjs-dashboard/app/dashboard/page.tsx
+++ nextjs-dashboard/app/dashboard/page.tsx
@@ -4,7 +4,7 @@
 
 import SideNav from '@/app/ui/dashboard/sidenav';
  
-export default function Layout({ children }: { children: React.ReactNode }) {
+export default function Layout({ children }: any) {
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
       <div className="w-full flex-none md:w-64">
